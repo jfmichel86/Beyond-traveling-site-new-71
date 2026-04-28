2549ebd4ad0a65726d3d3634a9b0f7e4d71c9478
jfmichel86 <jfmichel86@gmail.com>
2026-04-28 15:31:47 -0600

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,11 @@
+import { MetadataRoute } from "next";
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://www.beyondtravelingmx.com/sitemap.xml",
+  };
+}
