c62e2e6b00a36c079eb059dc7c57032bca11fcb0
jfmichel86 <jfmichel86@gmail.com>
2026-04-28 15:27:47 -0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,22 @@
+import { MetadataRoute } from "next";
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.beyondtravelingmx.com/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.beyondtravelingmx.com/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.beyondtravelingmx.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.beyondtravelingmx.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
