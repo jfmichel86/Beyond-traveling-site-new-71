bb286fd0038028bb0b57a9bf9a9b59381068ffe7
jfmichel86 <jfmichel86@gmail.com>
2026-06-04 18:34:37 -0600

    Create discover-translations.ts

---
+++ lib/translations/discover-translations.ts
@@ -0,0 +1,5 @@
+import { discoverTranslations } from "@/lib/translations/discover-translations";
+
+export const discoverTranslations: Record<string, string> = {
+  // Discover page translations will go here
+};
